1c2ee52216dd265d70acdcbc0e5c417e3e8277e2
Luiz Menezes <luiz.menezesf@gmail.com>
2017-01-31 11:22:58 -0200

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysimplemodel',
-    version='0.0.2',
+    version='0.0.3',
     description='Simple Models for Python',
     url='https://github.com/lamenezes/simple-model',
     author='Luiz Menezes',
