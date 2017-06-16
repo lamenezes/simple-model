5ea0f95f290f048ecc7cdcf570e2cd13c4035581
Luiz Menezes <luiz.menezesf@gmail.com>
2017-06-16 11:02:16 -0300

    Bump version: 0.8.4 → 0.8.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysimplemodel',
-    version='0.8.4',
+    version='0.8.5',
     description='Simple Models for Python',
     url='https://github.com/lamenezes/simple-model',
     author='Luiz Menezes',
