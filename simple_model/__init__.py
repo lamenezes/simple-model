9a57db8c7491338ec63d669ecde253213c42d8eb
Luiz Menezes <luiz.menezesf@gmail.com>
2018-01-11 10:36:38 -0200

    Add missing function model_many_builder() to simple_model.__init__

--- simple_model/__init__.py
+++ simple_model/__init__.py
@@ -1,4 +1,4 @@
-from .builder import model_builder
+from .builder import model_builder, model_many_builder
 from .models import Model
 
-__all__ = ('Model', 'model_builder')
+__all__ = ('Model', 'model_builder', 'model_many_builder')
